fd8cdc1ad532b3d361772087d4aaaa43e9ccce34
Thayne McCombs <thayne@lucidchart.com>
2018-04-24 13:58:32 -0600

    Add TracingAction traits

--- play/src/main/scala/io/opentracing/play/DefaultTracingFilter.scala
+++ play/src/main/scala/io/opentracing/play/DefaultTracingFilter.scala
@@ -4,6 +4,7 @@ import io.opentracing.contrib.global.GlobalTracer
 import io.opentracing.threadcontext.ContextSpan
 import scala.concurrent.ExecutionContext
 
+@deprecated("Filters don't work well for setting thread local storage, use DefaultTracingAction instead", "0.12")
 class DefaultTracingFilter(taggers: Traversable[SpanTagger])(implicit executionContext: ExecutionContext) extends TracingFilter(ContextSpan.DEFAULT, taggers) {
   protected[this] def tracer = GlobalTracer.get
 }
--- play/src/main/scala/io/opentracing/play/TracingFilter.scala
+++ play/src/main/scala/io/opentracing/play/TracingFilter.scala
@@ -8,11 +8,13 @@ import java.util.function.Supplier
 import play.api.mvc._
 import scala.concurrent.ExecutionContext
 
+@deprecated("Filters don't work well for setting thread local storage, use TracingAction instead", "0.12")
 abstract class TracingFilter(protected[this] val contextSpan: ContextSpan, taggers: Traversable[SpanTagger])(implicit ec: ExecutionContext) extends EssentialFilter {
 
   private[this] def toSupplier[A](a: => A) = new Supplier[A] { def get() = a }
 
   protected[this] def tracer: Tracer
+
 
   def apply(next: EssentialAction) = EssentialAction { request =>
     val span = tracer.buildSpan(Routes.endpointName(request).getOrElse(s"HTTP ${request.method}"))
