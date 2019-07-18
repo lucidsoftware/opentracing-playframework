7e9b9618299172d5afbbfb8b32efd91561a9b745
Zeimyth <zeimyth@hotmail.com>
2019-07-18 10:57:35 -0600

    Add full support for Play 2.6
    Deprecated tag-based TagsSpanTagger. Tags are deprecated in Play 2.6 and removed in Play 2.7. Replaced TagsSpanTagger with RequestAttributesSpanTagger. This allows an easy migration on Play 2.6 and enables future Play 2.7 support

--- play/src/main/scala/io/opentracing/play/TagsSpanTagger.scala
+++ play/src/main/scala/io/opentracing/play/TagsSpanTagger.scala
@@ -6,6 +6,7 @@ import play.api.mvc.{RequestHeader, Result}
 /**
  * Applies tags from [[RequestHeader#tags]]
  */
+@deprecated("Tags are deprecated in Play 2.6; use RequestAttributesSpanTagger instead", "0.14")
 class TagsSpanTagger(include: String => Boolean) extends SpanTagger {
   def tag(span: Span, request: RequestHeader, result: Result) = {
     request.tags.foreach { case (key, value) => if (include(key)) span.setTag(s"play.$key", value) }
