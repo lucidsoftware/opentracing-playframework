b6faf4ffc80435a73476b01c0911c3b86aa718d1
Sriraam AS <sriraam@lucidchart.com>
2021-06-04 15:00:56 -0400

    fix no staging repositories found issue

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "$PGP_SECRET" | base64 --decode | gpg --import
 if [[ $GITHUB_REF == refs/tags/* ]]; then
-  command="; publishSigned; sonatypeReleaseAll"
+  command="; publishSigned; sonatypeOpen; sonatypeReleaseAll"
 else
   command="publishSigned"
 fi
