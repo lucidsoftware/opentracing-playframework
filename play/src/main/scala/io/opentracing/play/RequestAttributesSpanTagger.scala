c04446f70e17efe41a07864220b57b554d38f818
Sriraam AS <sriraam@lucidchart.com>
2021-09-09 08:27:14 -0400

    fix scaladoc in requestattributesspantagger

--- play/src/main/scala/io/opentracing/play/RequestAttributesSpanTagger.scala
+++ play/src/main/scala/io/opentracing/play/RequestAttributesSpanTagger.scala
@@ -5,17 +5,17 @@ import play.api.libs.typedmap.TypedKey
 import play.api.mvc.{RequestHeader, Result}
 
 /**
- * Applies attributes from [[RequestHeader#attrs]].
+ * Applies attributes from [[https://www.playframework.com/documentation/2.8.x/api/scala/play/api/mvc/RequestHeader.html RequestHeader#attrs]].
  *
- * @param attributeKeys The set of attributes to read from the [[RequestHeader]]. Must be explicitly stated because
- *  [[RequestHeader#attrs]] does not support iterating over all attributes on a request.
+ * @param attributeKeys The set of attributes to read from the [[https://www.playframework.com/documentation/2.8.x/api/scala/play/api/mvc/RequestHeader.html  RequestHeader]]. Must be explicitly stated because
+ *  [[https://www.playframework.com/documentation/2.8.x/api/scala/play/api/mvc/RequestHeader.html RequestHeader#attrs]] does not support iterating over all attributes on a request.
  *
  *  Attributes may or may not have a displayName. In order to ensure that the tags set on the Span are sensible, only
  *  Attributes with a displayName set will be used to tag the Span.
  */
 class RequestAttributesSpanTagger(attributeKeys: Set[TypedKey[String]]) extends SpanTagger {
 
-  def tag(span: Span, request: RequestHeader, result: Option[Result]) =
+  def tag(span: Span, request: RequestHeader, result: Option[Result]): Unit =
     attributeKeys.foreach { key =>
       key.displayName.foreach { name =>
         request.attrs
